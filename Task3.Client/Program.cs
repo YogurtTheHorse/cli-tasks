cf48680b3e2dd660b376f8c73e15733a9e5badca
yegorf1 <yegorf1@gmail.com>
2018-03-04 16:01:52 +0300

    Removed useless instructions from third task

--- Task3.Client/Program.cs
+++ Task3.Client/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using Task3.ShareCode;
 
 namespace Task3.Client {
@@ -9,13 +8,10 @@ namespace Task3.Client {
 			io.OnImcomingTextMessage += (s, e) => Console.Write(e.Message);
 
 			io.StartListeningAsync();
-			//io.Write("spam", NetworkMessageType.Status);
 
 			while (io.IsListening) {
 				io.WriteLine(Console.ReadLine());
 			}
-
-			Console.ReadLine();
 		}
 	}
 }
